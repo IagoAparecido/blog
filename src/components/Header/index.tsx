957432105fa6c32c08696dde30939f84b27f469a
Iago <100394244+IagoAparecido@users.noreply.github.com>
2023-05-08 10:49:16 -0400

    ajustes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import "./style.css";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { getAuth, User } from "firebase/auth";
 import { logout } from "../../firebase";
@@ -10,9 +10,21 @@ import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 function Header() {
   const [user, setUser] = useState<User | null>(null);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   useEffect(() => {
     const auth = getAuth();
@@ -36,20 +48,53 @@ function Header() {
           noWrap
           sx={{ flex: 1 }}
         >
-          Blog
+          <a href="/" style={{ color: "black" }}>
+            Blog
+          </a>
         </Typography>
         <IconButton>
           <SearchIcon />
         </IconButton>
-        <Button className="user_config" variant="outlined" size="small">
-          {user ? (
-            <span onClick={() => logout()}>SIGN OUT</span>
-          ) : (
+
+        {user ? (
+          <>
+            <Button
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              <AccountCircleIcon
+                component="svg"
+                color="primary"
+                fontSize="large"
+                className="user_config"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              />
+            </Button>
+
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem onClick={() => logout()}>Logout</MenuItem>
+            </Menu>
+          </>
+        ) : (
+          <Button className="user_config" variant="outlined" size="small">
             <a className="link_login" href="/login">
               SIGN IN
             </a>
-          )}
-        </Button>
+          </Button>
+        )}
       </Toolbar>
     </Container>
   );
